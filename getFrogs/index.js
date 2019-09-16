108253ec3ee491865c06e25fd6ad252fcfd04bbe
Thirumalaa Srinivas <srinivassekar@gmail.com>
2019-09-15 22:19:26 -0500

    first commit I guess

---
+++ getFrogs/index.js
@@ -0,0 +1,8 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: ["Australian Green Tree Frog", "White-lipped Tree Frog", "Cane Toad", "Red-eyed Tree Frog"]
+    };
+};
